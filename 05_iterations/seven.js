ba4640f9d3e9c67627a3f691e3ff0e06b3050001
Samarburnwal <104635928+Samarburnwal@users.noreply.github.com>
2024-08-09 03:14:23 +0000

    chaining

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+const myNums = myNumbers.map((num)=> num+10);
+const myNums1 = myNumbers.map((num)=> num>4);
+
+console.log(myNums);
+console.log(myNums1);
+
+const specialNums = myNumbers
+                    .map((elem)=> elem+10)
+                    .filter((elem)=>elem>=14)
+                    .map((e)=> e*10);
+
+console.log(specialNums);
